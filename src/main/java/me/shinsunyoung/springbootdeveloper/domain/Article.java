e1004e576445cdcfdd1444f6eb0bcd7eae46585a
logun01 <logun0128@gmail.com>
2025-09-25 18:36:57 +0900

    Initial commit

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -0,0 +1,31 @@
+package me.shinsunyoung.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity//엔티티로 지정
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키를 자동으로 1씩 증가
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,19 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+import me.shinsunyoung.springbootdeveloper.dto.AddArticleRequest;
+import me.shinsunyoung.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service //빈으로 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    //블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
